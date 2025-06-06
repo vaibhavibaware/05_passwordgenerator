85b785a808cb1729cf3eea0b7fe689ead91296e1
Vaibhavi Baware <vaibhavibaware09@gmail.com>
2025-06-06 13:24:14 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}" // ✅ Scan all relevant files
-  ],
+ 
   theme: {
     extend: {},
   },
